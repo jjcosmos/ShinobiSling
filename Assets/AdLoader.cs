081b276c22c28c533b3ec28c22ffd4e21bd4ecba
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2019-06-07 22:55:52 -0700

    commit before changing camera

--- Assets/AdLoader.cs
+++ Assets/AdLoader.cs
@@ -1,32 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Monetization;
 using UnityEngine.Advertisements;
-
 public class AdLoader : MonoBehaviour
 {
     // Start is called before the first frame update
+    // 3177854
+    // BannerAd
     public bool isAdShowing;
+    private string store_id = "3177854";
+    private string banner_ad = "BannerAd";
+    private string reward_ad = "rewardedVideo";
+
+    [SerializeField] bool testModeOn;
+
     void Start()
     {
-        Advertisement.Initialize("3177854");
         isAdShowing = false;
+        Monetization.Initialize(store_id, testModeOn);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
-        if (!isAdShowing && Advertisement.IsReady("video"))
-        {
-
-            Advertisement.Show("video");
-            isAdShowing = true;
-        }
-        else
-        {
-            Debug.Log(Advertisement.IsReady("video").ToString());
-        }
-        
     }
+
+    public void ShowAd()
+    {
+        if (Monetization.IsReady(reward_ad))
+        {
+            ShowAdPlacementContent ad = null;
+            ad = Monetization.GetPlacementContent(banner_ad) as ShowAdPlacementContent;
+
+            if (ad != null)
+            {
+                ad.Show();
+                isAdShowing = true;
+                Debug.Log(ad.placementId);
+            }
+        }
+    }
+
+    
+
 }
+
+    
