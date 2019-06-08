195dfec7b5415de130faa5f39cf402969c012ec1
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2019-06-07 19:16:59 -0700

    Try With Unity Ads

---
+++ Assets/AdLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool isAdShowing;
+    void Start()
+    {
+        Advertisement.Initialize("3177854");
+        isAdShowing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAdShowing && Advertisement.IsReady("OnMenu"))
+        {
+
+            Advertisement.Show("OnMenu");
+            isAdShowing = true;
+        }
+    }
+}
