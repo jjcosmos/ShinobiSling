2ad7ab6c6570b3c70747bbf92a26be129ec56cb6
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2019-06-07 20:31:39 -0700

    upload before chaos

--- Assets/AdLoader.cs
+++ Assets/AdLoader.cs
@@ -6,7 +6,7 @@ using UnityEngine.Advertisements;
 public class AdLoader : MonoBehaviour
 {
     // Start is called before the first frame update
-    bool isAdShowing;
+    public bool isAdShowing;
     void Start()
     {
         Advertisement.Initialize("3177854");
@@ -16,11 +16,17 @@ public class AdLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!isAdShowing && Advertisement.IsReady("OnMenu"))
+        
+        if (!isAdShowing && Advertisement.IsReady("video"))
         {
 
-            Advertisement.Show("OnMenu");
+            Advertisement.Show("video");
             isAdShowing = true;
         }
+        else
+        {
+            Debug.Log(Advertisement.IsReady("video").ToString());
+        }
+        
     }
 }
